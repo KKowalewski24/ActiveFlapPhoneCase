30179596981731a701caa5e4fcbb8cf3f52420ee
Kamil Kowalewski <szamil24@gmail.com>
2020-10-22 19:53:00 +0200

    Add turning off screen

--- app/src/main/java/pl/kkowalewski/activeflap/MainActivity.java
+++ app/src/main/java/pl/kkowalewski/activeflap/MainActivity.java
@@ -1,11 +1,17 @@
 package pl.kkowalewski.activeflap;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,19 +19,49 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     /*------------------------ FIELDS REGION ------------------------*/
     private static final float PROXIMITY_THRESHOLD = 1.0f;
+    private static final int RESULT_ENABLE = 1;
 
     private TextView textView;
+    private Button adminButton;
     private SensorManager sensorManager;
     private Sensor proximitySensor;
+    private DevicePolicyManager deviceManger;
+    private ComponentName componentName;
 
     /*------------------------ METHODS REGION ------------------------*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adminButton = findViewById(R.id.adminButton);
         textView = findViewById(R.id.textView);
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        deviceManger = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        componentName = new ComponentName(this, Admin.class);
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+                startActivityForResult(intent, RESULT_ENABLE);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(
+                this, proximitySensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     @Override
@@ -33,10 +69,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             final float distance = event.values[0];
 
-            if (distance <= PROXIMITY_THRESHOLD) {
-                textView.setText("close");
+            if (distance <= PROXIMITY_THRESHOLD && deviceManger.isAdminActive(componentName)) {
+                deviceManger.lockNow();
             } else {
-                textView.setText("far");
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
             }
         }
     }
@@ -44,17 +80,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_FASTEST);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
     }
 }
