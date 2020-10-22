90687ba126c1384fd95fecf4d4cd13f52310c637
Kamil Kowalewski <szamil24@gmail.com>
2020-10-22 14:06:18 +0200

    Add reading values from light sensor

--- app/src/main/java/pl/kkowalewski/activeflap/MainActivity.java
+++ app/src/main/java/pl/kkowalewski/activeflap/MainActivity.java
@@ -1,13 +1,52 @@
 package pl.kkowalewski.activeflap;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    /*------------------------ FIELDS REGION ------------------------*/
+    private TextView textView;
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+
+    /*------------------------ METHODS REGION ------------------------*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            textView.setText("light: " + event.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
