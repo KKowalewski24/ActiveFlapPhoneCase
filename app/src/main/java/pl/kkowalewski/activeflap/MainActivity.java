1c229186dbfc302308a1ba82c0c89993f92e2f30
Kamil Kowalewski <szamil24@gmail.com>
2020-10-22 20:13:40 +0200

    Improve code

--- app/src/main/java/pl/kkowalewski/activeflap/MainActivity.java
+++ app/src/main/java/pl/kkowalewski/activeflap/MainActivity.java
@@ -12,7 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -20,9 +20,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     /*------------------------ FIELDS REGION ------------------------*/
     private static final float PROXIMITY_THRESHOLD = 1.0f;
     private static final int RESULT_ENABLE = 1;
+    public static final String ADD_ADMIN_PRIVILEGES = "Add Admin Privileges";
+    public static final String REMOVE_ADMIN_PRIVILEGES = "Remove Admin Privileges";
 
-    private TextView textView;
-    private Button adminButton;
+    private Button adminButtonEnable;
+    private Button adminButtonDisable;
     private SensorManager sensorManager;
     private Sensor proximitySensor;
     private DevicePolicyManager deviceManger;
@@ -33,29 +35,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adminButton = findViewById(R.id.adminButton);
-        textView = findViewById(R.id.textView);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        deviceManger = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-        componentName = new ComponentName(this, Admin.class);
-
-        adminButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
-                startActivityForResult(intent, RESULT_ENABLE);
-            }
-        });
+        fieldSetup();
+        buttonSetup();
+        onClickSetup();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(
-                this, proximitySensor, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, proximitySensor,
+                SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -66,19 +56,63 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY
+                && deviceManger.isAdminActive(componentName)) {
             final float distance = event.values[0];
 
-            if (distance <= PROXIMITY_THRESHOLD && deviceManger.isAdminActive(componentName)) {
+            if (distance <= PROXIMITY_THRESHOLD) {
                 deviceManger.lockNow();
             } else {
                 getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
             }
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    ADD_ADMIN_PRIVILEGES, Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    private void buttonSetup() {
+        adminButtonEnable = findViewById(R.id.adminButtonEnable);
+        adminButtonDisable = findViewById(R.id.adminButtonDisable);
+        adminButtonEnable.setText(ADD_ADMIN_PRIVILEGES);
+        adminButtonDisable.setText(REMOVE_ADMIN_PRIVILEGES);
+
+        boolean isAdmin = deviceManger.isAdminActive(componentName);
+        adminButtonEnable.setVisibility(isAdmin ? View.GONE : View.VISIBLE);
+        adminButtonDisable.setVisibility(isAdmin ? View.VISIBLE : View.GONE);
+    }
+
+    private void fieldSetup() {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        deviceManger = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        componentName = new ComponentName(this, Admin.class);
+    }
+
+    private void onClickSetup() {
+        adminButtonEnable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+                startActivityForResult(intent, RESULT_ENABLE);
+                adminButtonDisable.setVisibility(View.VISIBLE);
+                adminButtonEnable.setVisibility(View.GONE);
+            }
+        });
+
+        adminButtonDisable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deviceManger.removeActiveAdmin(componentName);
+                adminButtonDisable.setVisibility(View.GONE);
+                adminButtonEnable.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
